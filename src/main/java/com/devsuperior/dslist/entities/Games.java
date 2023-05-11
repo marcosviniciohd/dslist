a5718c7952ae1b15eb24b89331b3bba5280410dd
Marcos <marcosviniciohd@hotmail.com>
2023-05-11 00:04:49 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Games.java
+++ src/main/java/com/devsuperior/dslist/entities/Games.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 import java.util.Objects;
 @Entity
-@Table(name = "tb_games")
+@Table(name = "tb_game")
 public class Games {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,20 +13,24 @@ public class Games {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Games() {
     }
 
-    public Games(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Games(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -64,12 +68,12 @@ public class Games {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public String getImgUrl() {
@@ -95,6 +99,15 @@ public class Games {
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
 
     @Override
     public boolean equals(Object o) {
